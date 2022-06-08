d4fe30037199dc8884303d2136d7f8a0925476d5
Ya-dola <Ya-dola@users.noreply.github.com>
2022-06-08 22:34:10 +0530

    Added Base Snake Movement

---
+++ Assets/Scripts/Snek.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Snek : MonoBehaviour
+{
+    public Vector2 moveDir;
+
+    public float speed;
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveDir = context.ReadValue<Vector2>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, new Vector3(
+            Mathf.Round(transform.position.x) + moveDir.x,
+            Mathf.Ceil(transform.position.y) + moveDir.y,
+            transform.position.z), Time.fixedDeltaTime * speed);
+    }
+}
