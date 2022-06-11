89a36fde47d7495078cb62d870a828df05ad8849
Ya-dola <Ya-dola@users.noreply.github.com>
2022-06-11 22:41:01 +0530

    Added Persistent and Smooth Movement

--- Assets/Scripts/Snek.cs
+++ Assets/Scripts/Snek.cs
@@ -6,18 +6,25 @@ using UnityEngine.InputSystem;
 
 public class Snek : MonoBehaviour
 {
-    public Vector2 moveDir;
+    [Header("Movement Vectors")]
+    public Vector2 newMoveDir;
 
-    public float speed;
+    public Vector2 currentMoveDir;
 
+    [Header("Movement Smoothness")]
+    public float smoothStep = 0.1f;
+
+    // Read Value of Movement from New Input System
     public void OnMove(InputAction.CallbackContext context)
     {
-        moveDir = context.ReadValue<Vector2>();
+        newMoveDir = context.ReadValue<Vector2>();
     }
 
     // Start is called before the first frame update
     void Start()
     {
+        // Movement Coroutine
+        StartCoroutine(MoveSnekConfigRout());
     }
 
     // Update is called once per frame
@@ -27,9 +34,67 @@ public class Snek : MonoBehaviour
 
     private void FixedUpdate()
     {
-        transform.position = Vector3.Lerp(transform.position, new Vector3(
-            Mathf.Round(transform.position.x) + moveDir.x,
-            Mathf.Round(transform.position.y) + moveDir.y,
-            transform.position.z), Time.fixedDeltaTime * speed);
+        ChangeDir();
+    }
+
+    // Change Direction of Movement and Keep it Persistent
+    private void ChangeDir()
+    {
+        // Up Direction
+        if (newMoveDir.Equals(new Vector2(0f, 1f)))
+        {
+            currentMoveDir = new Vector2(0f, 1f);
+        }
+
+        // Down Direction
+        if (newMoveDir.Equals(new Vector2(0f, -1f)))
+        {
+            currentMoveDir = new Vector2(0f, -1f);
+        }
+
+        // Left Direction
+        if (newMoveDir.Equals(new Vector2(-1f, 0f)))
+        {
+            currentMoveDir = new Vector2(-1f, 0f);
+        }
+
+        // Right Direction
+        if (newMoveDir.Equals(new Vector2(1f, 0f)))
+        {
+            currentMoveDir = new Vector2(1f, 0f);
+        }
+    }
+
+    IEnumerator MoveSnekConfigRout()
+    {
+        while (true)
+        {
+            // Calls for Lerping Movement after Movement finishes
+            yield return StartCoroutine(MoveSnekRout(
+                new Vector3(Mathf.Round(transform.position.x) + currentMoveDir.x,
+                    Mathf.Round(transform.position.y) + currentMoveDir.y,
+                    transform.position.z), smoothStep));
+        }
+    }
+
+    IEnumerator MoveSnekRout(Vector3 newPos, float duration)
+    {
+        // Timer for Lerp
+        float time = 0;
+
+        // Total time allocated for Lerp
+        while (time < duration)
+        {
+            // Division of Time Elapsed by total time allocated for lerp
+            transform.position = Vector3.Lerp(transform.position, newPos, time / duration);
+
+            // Incrementing timer on Every Frame for Lerp
+            time += Time.fixedDeltaTime;
+
+            yield return null;
+        }
+
+        // Ensure New Position is the Final Position after Lerp
+        transform.position = newPos;
     }
 }
